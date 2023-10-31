42ba32cbe8047db380d2149b203838bc58f0d7be
Mohamad <ima.mawaldi@gmail.com>
2023-10-31 23:55:49 +0100

    - Created AnimalFinder app with MlModel

---
+++ AnimalFinder/DoModel.swift
@@ -0,0 +1,58 @@
+//
+//  DoModel.swift
+//  AnimalFinder
+//
+//  Created by mohamad on 2023-10-31.
+//
+
+import Vision
+import Foundation
+import UIKit
+
+class DoModel {
+    
+    func doImage( from animalImage: UIImage ) -> AnimalImageFinderOutput? {
+        
+        let defaultConfig = MLModelConfiguration()
+        
+        let imageClassifierWrapper = try? AnimalImageFinder(configuration: defaultConfig)
+        
+        let theimageBuffer = buffer(from: animalImage)!
+        
+        do {
+            return try imageClassifierWrapper!.prediction(image: theimageBuffer)
+                        
+        } catch {
+            // error
+        }
+        return nil
+        
+    }
+
+    
+    
+    func buffer(from image: UIImage) -> CVPixelBuffer? {
+      let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+      var pixelBuffer : CVPixelBuffer?
+      let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+      guard (status == kCVReturnSuccess) else {
+        return nil
+      }
+
+      CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+      let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+
+      let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+      let context = CGContext(data: pixelData, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+
+      context?.translateBy(x: 0, y: image.size.height)
+      context?.scaleBy(x: 1.0, y: -1.0)
+
+      UIGraphicsPushContext(context!)
+      image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+      UIGraphicsPopContext()
+      CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+
+      return pixelBuffer
+    }
+}

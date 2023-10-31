07a8bb4b209f1bed348475cbd5f35257e9fb78b7
Mohamad <ima.mawaldi@gmail.com>
2023-10-31 22:45:49 +0100

    Initial Commit

---
+++ AnimalFinder/AnimalFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFinderApp.swift
+//  AnimalFinder
+//
+//  Created by mohamad on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
